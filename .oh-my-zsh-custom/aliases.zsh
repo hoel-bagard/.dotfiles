895a84bff9802dfc80b6664ebdeb37a4526021e1
Hoel Bagard <hoel.bagard@gmail.com>
2022-10-01 10:24:45 +0900

    unmount NAS commands now also spin down disk.

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -18,9 +18,9 @@ alias disconnect-vpn="pkill -SIGTERM -f 'openvpn'"
 
 # Mount home NAS
 alias mount-home-public="sudo mount -t cifs //192.168.0.245/PublicNAS /mnt/nas_public -o uid=$(id -u),gid=$(id -g)"
-alias unmount-home-public="sudo umount /mnt/nas_public"
+alias unmount-home-public="sudo umount /mnt/nas_public; ssh -t hoel@192.168.0.245 'sudo hdparm -y /dev/sda'"
 alias mount-home-private="sudo mount -t cifs //192.168.0.245/PrivateNAS /mnt/nas_private -o username=hoel,uid=$(id -u),gid=$(id -g)"
-alias unmount-home-private="sudo umount /mnt/nas_private"
+alias unmount-home-private="sudo umount /mnt/nas_private; ssh -t hoel@192.168.0.245 'sudo hdparm -y /dev/sdb'"
 
 # Everyday commands
 alias ll="exa -lahFG"
