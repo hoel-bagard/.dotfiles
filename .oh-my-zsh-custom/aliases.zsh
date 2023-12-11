6275afb642593b8b9da8294913164c2ddc83410b
Hoel Bagard <hoel.bagard@gmail.com>
2023-12-11 11:19:57 +0900

    Add merge to gitconfig.

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,21 +1,3 @@
-# Work
-alias mount-bitter="sshfs bitter.xc:/home/hoel ~/mount/bitter -o dcache_timeout=3"
-alias unmount-bitter="sudo umount ~/mount/bitter"
-alias mount-black="sshfs black.xc:/home/hoel ~/mount/black -o dcache_timeout=3"
-alias unmount-black="sudo umount ~/mount/black"
-alias mount-3090="sshfs 3090.xc:/home/hoel ~/mount/3090 -o dcache_timeout=3"
-alias unmount-3090="sudo umount ~/mount/3090"
-alias mount-dgx="sshfs dgx.xc:/home/hoel ~/mount/dgx -o dcache_timeout=3"
-alias unmount-dgx="sudo umount ~/mount/dgx"
-alias mount-censei="sshfs censei.xc:/home/hoel ~/mount/censei -o dcache_timeout=3"
-alias unmount-censei="sudo umount ~/mount/censei"
-alias mount-nas-project="sudo mount -t cifs //192.168.100.95/pub/ ~/mount/project_NAS/ -o username=guest01,uid=$(id -u),gid=$(id -g),password=Ilovecurry1"
-alias unmount-nas-project="sudo umount ~/mount/project_NAS"
-alias mount-nas2-project="sudo mount -t cifs //192.168.100.150/xc_backup/ ~/mount/project_NAS2/ -o username=guest01,uid=$(id -u),gid=$(id -g),password=Ilovecurry1"
-alias unmount-nas2-project="sudo umount ~/mount/project_NAS2"
-alias connect-vpn="sudo openvpn --config /etc/openvpn/client/xc_client.ovpn --auth-user-pass /etc/openvpn/client/credentials.txt --data-ciphers 'AES-128-CBC' &"
-alias disconnect-vpn="sudo pkill -SIGTERM -f 'openvpn'"
-
 # Mount home NAS
 alias mount-home-public="sudo mount -t cifs //192.168.0.245/PublicNAS /mnt/nas_public -o uid=$(id -u),gid=$(id -g)"
 alias unmount-home-public="sudo umount /mnt/nas_public; ssh -t hoel@192.168.0.245 'sudo hdparm -y /dev/sda'"
@@ -36,7 +18,6 @@ alias duc="du -hcxd1 | sort -hr"  # du -shcx /home/* | sort -hr
 # alias cpr="rsync -pogbr -hhh --backup-dir=/tmp/rsync -e /dev/null --progress"  # See https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/cp for details
 alias cpr="rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1"  # From https://wiki.archlinux.org/title/rsync
 alias mvr="rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1 --remove-source-files "
-alias librecalc="libreoffice --calc --safe-mode"
 # Git
 alias gc="git c"
 alias gcm="git cm"
