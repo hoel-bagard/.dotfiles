707be72d0b41222ce69fdcbe67d8b70f68132f26
Hoel Bagard <hoel.bagard@gmail.com>
2024-02-24 20:34:35 +0900

    Add mkfile alias/function/command.

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -13,7 +13,8 @@ else
     alias ll="ls -lah"
 fi
 alias ls='ls --color=auto'
-alias mkdir="mkdir -p" # Create missing folders if needed
+alias mkdir="mkdir -p"
+mkfile() { mkdir -p "$(dirname "$1")" && touch "$1"; }
 alias .1="cd .."
 alias .2="cd ../.."
 alias .3="cd ../../.."
