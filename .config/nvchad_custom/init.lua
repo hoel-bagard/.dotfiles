8105febd7ef7c979c541479b9a84e287c92ad76b
Hoel Bagard <hoel.bagard@gmail.com>
2024-04-03 11:43:14 +0900

    temp clipboard fix for wsl

--- .config/nvchad_custom/init.lua
+++ .config/nvchad_custom/init.lua
@@ -5,3 +5,19 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+
+if vim.fn.has("wsl") then
+    vim.g.clipboard = {
+        name = "clip.exe (Copy Only)",
+        copy = {
+            ["+"] = "clip.exe",
+            ["*"] = "clip.exe"
+        },
+        paste = {
+            ["+"] = "clip.exe",
+            ["*"] = "clip.exe"
+        },
+        cache_enabled = true
+    }
+end
