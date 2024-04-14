a2fbcebc2af9feede9ac03198ae4b16a96c7fef5
Hoel Bagard <hoel.bagard@gmail.com>
2024-04-15 08:49:31 +0900

    nvim: remove wsl specific clipboard config.

--- .config/nvchad_custom/init.lua
+++ .config/nvchad_custom/init.lua
@@ -6,18 +6,18 @@
 --   command = "tabdo wincmd =",
 -- })
 
-
-if vim.fn.has("wsl") then
-    vim.g.clipboard = {
-        name = "clip.exe (Copy Only)",
-        copy = {
-            ["+"] = "clip.exe",
-            ["*"] = "clip.exe"
-        },
-        paste = {
-            ["+"] = "clip.exe",
-            ["*"] = "clip.exe"
-        },
-        cache_enabled = true
-    }
-end
+--
+-- if vim.fn.has("wsl") then
+--     vim.g.clipboard = {
+--         name = "clip.exe (Copy Only)",
+--         copy = {
+--             ["+"] = "clip.exe",
+--             ["*"] = "clip.exe"
+--         },
+--         paste = {
+--             ["+"] = "clip.exe",
+--             ["*"] = "clip.exe"
+--         },
+--         cache_enabled = true
+--     }
+-- end
