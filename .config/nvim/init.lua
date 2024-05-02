6be224f973ce2e81449730a0b5e32147a1ccc8b0
Hoel Bagard <hoel.bagard@gmail.com>
2024-05-02 20:28:00 +0900

    nvim: remove empty opts being passed to lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,6 +16,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("hoel.plugins", opts)
+require("lazy").setup("hoel.plugins")
 require("hoel.settings")
 require("hoel.keymaps")
