b8873252b2268626480c8d5d14a3dcb157857f4e
Hoel Bagard <hoel.bagard@gmail.com>
2024-05-06 20:25:53 +0900

    nvim: add rust_analyzer to mason ensure installed list

--- .config/nvim/lua/hoel/plugins/lsp.lua
+++ .config/nvim/lua/hoel/plugins/lsp.lua
@@ -126,6 +126,7 @@ return {
         -- for you, so that they are available from within Neovim.
         local ensure_installed = vim.tbl_keys(servers or {})
         vim.list_extend(ensure_installed, {
+            "rust_analyzer",
             "stylua", -- Used to format Lua code
             "hadolint",
             "jsonlint",
