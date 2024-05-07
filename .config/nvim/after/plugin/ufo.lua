d988d9f5fb2d1a5bf80095d673dafa5e60220725
Hoel Bagard <hoel.bagard@gmail.com>
2024-05-07 17:49:10 +0900

    nvim: use treesitter instead of lsp as provider for ufo

--- .config/nvim/after/plugin/ufo.lua
+++ .config/nvim/after/plugin/ufo.lua
@@ -3,19 +3,9 @@ vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to de
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
 
--- Tell the server the capability of foldingRange,
--- Neovim hasn't added foldingRange to default capabilities, users must add it manually.
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true,
-}
-
-local language_servers = require("lspconfig").util.available_servers()
-for _, ls in ipairs(language_servers) do
-    require("lspconfig")[ls].setup({
-        capabilities = capabilities,
-    })
-end
-
-require("ufo").setup()
+---@diagnostic disable-next-line: missing-fields
+require("ufo").setup({
+    provider_selector = function(bufnr, filetype, buftype)
+        return { "treesitter", "indent" }
+    end,
+})
