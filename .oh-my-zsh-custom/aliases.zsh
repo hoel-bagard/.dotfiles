cea11ba29deae51a73a5852b71d9fc378ad31177
Hoel Bagard <hoel.bagard@gmail.com>
2024-12-09 17:53:49 +0900

    aliases: modify path of the count-hours file

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -75,8 +75,8 @@ alias enable-boost="sudo sh -c \"echo 1 >> /sys/devices/system/cpu/cpufreq/boost
 alias git-config="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
 # Work
-alias xc-start="count-hours log start ~/work/xc/.count-hours.csv"
-alias xc-end="count-hours log end ~/work/xc/.count-hours.csv"
+alias xc-start="count-hours log start ~/work/xc/xc-hours/count-hours.csv"
+alias xc-end="count-hours log end ~/work/xc/xc-hours/.count-hours.csv"
 alias xc-source="source $HOME/work/xc/xc-aliases/xc-aliases.zsh"
 
 # Misc
