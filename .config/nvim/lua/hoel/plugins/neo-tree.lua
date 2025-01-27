e824ff2e191cab9b8b0278d62c3338f240153101
Hoel Bagard <hoel.bagard@gmail.com>
2025-01-27 17:22:49 +0900

    nvim: update neo-tree, add preview (image preview still not working)

--- .config/nvim/lua/hoel/plugins/neo-tree.lua
+++ .config/nvim/lua/hoel/plugins/neo-tree.lua
@@ -1,11 +1,26 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+    -- branch = "v3.x",
+    branch = "main",
+    opts = {
+        window = {
+            mappings = {
+                ["P"] = {
+                    "toggle_preview",
+                    config = {
+                        use_float = false,
+                        use_image_nvim = true,
+                        -- title = 'Neo-tree Preview',
+                    },
+                },
+            },
+        },
+    },
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window.
+        { "3rd/image.nvim", opts = {} }, -- Optional image support in preview window.
     },
     cmd = "Neotree",
 }
