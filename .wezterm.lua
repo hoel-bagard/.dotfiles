e07a4a437c93eadd71a55fc27874b01b98fb2d01
Hoel Bagard <hoel.bagard@gmail.com>
2025-01-27 17:09:15 +0900

    auto-format wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,20 +1,22 @@
 -- Mostly taken from https://github.com/theopn/dotfiles/tree/main  (and from the docs)
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local act = wezterm.action
 local mux = wezterm.mux
 
 local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will help provide clearer error messages
-if wezterm.config_builder then config = wezterm.config_builder() end
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
 
 -- Settings
-config.font = wezterm.font("MesloLGS NF", {weight="Regular", italic=false})
-config.font = wezterm.font_with_fallback {'JetbrainsMono Nerd Font'}
+config.font = wezterm.font("MesloLGS NF", { weight = "Regular", italic = false })
+config.font = wezterm.font_with_fallback({ "JetbrainsMono Nerd Font" })
 
 config.color_scheme = "Breeze (Gogh)"
 config.colors = {
-  cursor_fg = 'black',
+	cursor_fg = "black",
 }
 config.tab_bar_at_bottom = true
 config.default_cwd = "~"
@@ -27,196 +29,200 @@ config.default_workspace = "main"
 
 -- Dim inactive panes
 config.inactive_pane_hsb = {
-  saturation = 0.24,
-  brightness = 0.5
+	saturation = 0.24,
+	brightness = 0.5,
 }
 
 -- Keys
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-  -- Send C-a when pressing C-a twice
-  { key = "a",          mods = "LEADER|CTRL", action = act.SendKey { key = "a", mods = "CTRL" } },
-  { key = "c",          mods = "LEADER",      action = act.ActivateCopyMode },
-  { key = "phys:Space", mods = "LEADER",      action = act.ActivateCommandPalette },
+	-- Send C-a when pressing C-a twice
+	{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
 
-  -- Pane keybindings
-  { key = "s",          mods = "LEADER",      action = act.SplitVertical { domain = "CurrentPaneDomain" } },
-  { key = "v",          mods = "LEADER",      action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
-  { key = "h",          mods = "LEADER",      action = act.ActivatePaneDirection("Left") },
-  { key = "j",          mods = "LEADER",      action = act.ActivatePaneDirection("Down") },
-  { key = "k",          mods = "LEADER",      action = act.ActivatePaneDirection("Up") },
-  { key = "l",          mods = "LEADER",      action = act.ActivatePaneDirection("Right") },
-  { key = "q",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = true } },
-  { key = "z",          mods = "LEADER",      action = act.TogglePaneZoomState },
-  { key = "o",          mods = "LEADER",      action = act.RotatePanes "Clockwise" },
-  -- We can make separate keybindings for resizing panes
-  -- But Wezterm offers custom "mode" in the name of "KeyTable"
-  { key = "r",          mods = "LEADER",      action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+	-- Pane keybindings
+	{ key = "s", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "v", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "q", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+	{ key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+	-- We can make separate keybindings for resizing panes
+	-- But Wezterm offers custom "mode" in the name of "KeyTable"
+	{
+		key = "r",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+	},
 
-  -- Tab keybindings
-  { key = "t",          mods = "LEADER",      action = act.SpawnTab("CurrentPaneDomain") },
-  { key = "d",          mods = "LEADER",      action = act.ActivateTabRelative(-1) },
-  { key = "f",          mods = "LEADER",      action = act.ActivateTabRelative(1) },
-  { key = "n",          mods = "LEADER",      action = act.ShowTabNavigator },
-  {
-    key = "e",
-    mods = "LEADER",
-    action = act.PromptInputLine {
-      description = wezterm.format {
-        { Attribute = { Intensity = "Bold" } },
-        { Foreground = { AnsiColor = "Fuchsia" } },
-        { Text = "Renaming Tab Title...:" },
-      },
-      action = wezterm.action_callback(function(window, pane, line)
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end)
-    }
-  },
-  -- Key table for moving tabs around
-  { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
-  -- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
-  { key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
-  { key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
+	-- Tab keybindings
+	{ key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "d", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "f", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "n", mods = "LEADER", action = act.ShowTabNavigator },
+	{
+		key = "e",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Renaming Tab Title...:" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	-- Key table for moving tabs around
+	{ key = "m", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
+	-- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
+	{ key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
+	{ key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
 
-  -- Lastly, workspace
-  { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+	-- Lastly, workspace
+	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
 }
 
 config.key_tables = {
-  resize_pane = {
-    { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
-    { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
-    { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
-    { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
-    { key = "Escape", action = "PopKeyTable" },
-    { key = "Enter",  action = "PopKeyTable" },
-  },
-  move_tab = {
-    { key = "h",      action = act.MoveTabRelative(-1) },
-    { key = "j",      action = act.MoveTabRelative(-1) },
-    { key = "k",      action = act.MoveTabRelative(1) },
-    { key = "l",      action = act.MoveTabRelative(1) },
-    { key = "Escape", action = "PopKeyTable" },
-    { key = "Enter",  action = "PopKeyTable" },
-  }
+	resize_pane = {
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+	move_tab = {
+		{ key = "h", action = act.MoveTabRelative(-1) },
+		{ key = "j", action = act.MoveTabRelative(-1) },
+		{ key = "k", action = act.MoveTabRelative(1) },
+		{ key = "l", action = act.MoveTabRelative(1) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
 }
 
 -- Allow navigating the tabs with ALT+index.
 for i = 1, 9 do
-  table.insert(config.keys, {
-                 key = tostring(i),
-                 mods = "ALT",
-                 action = act.ActivateTab(i - 1)
-  })
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "ALT",
+		action = act.ActivateTab(i - 1),
+	})
 end
 
 -- Does not work, at least on WSL, see https://wezfurlong.org/wezterm/config/lua/keyassignment/SelectTextAtMouseCursor.html
 config.mouse_bindings = {
-  {
-    event = { Down = { streak = 3, button = 'Left' } },
-    action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
-    mods = 'NONE',
-  },
+	{
+		event = { Down = { streak = 3, button = "Left" } },
+		action = wezterm.action.SelectTextAtMouseCursor("SemanticZone"),
+		mods = "NONE",
+	},
 }
 
 -- Set bottom right
-wezterm.on('update-right-status', function(window, pane)
-  -- Each element holds the text for a cell in a "powerline" style << fade
-  local cells = {}
+wezterm.on("update-right-status", function(window, pane)
+	-- Each element holds the text for a cell in a "powerline" style << fade
+	local cells = {}
 
-  -- Figure out the cwd and host of the current pane.
-  -- This will pick up the hostname for the remote host if your
-  -- shell is using OSC 7 on the remote host.
-  local cwd_uri = pane:get_current_working_dir()
-  if cwd_uri then
-    local cwd = ''
-    local hostname = ''
+	-- Figure out the cwd and host of the current pane.
+	-- This will pick up the hostname for the remote host if your
+	-- shell is using OSC 7 on the remote host.
+	local cwd_uri = pane:get_current_working_dir()
+	if cwd_uri then
+		local cwd = ""
+		local hostname = ""
 
-    if type(cwd_uri) == 'userdata' then
-      -- Running on a newer version of wezterm and we have
-      -- a URL object here, making this simple!
+		if type(cwd_uri) == "userdata" then
+			-- Running on a newer version of wezterm and we have
+			-- a URL object here, making this simple!
 
-      cwd = cwd_uri.file_path
-      hostname = cwd_uri.host or wezterm.hostname()
-    else
-      -- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
-      -- which doesn't have the Url object
-      cwd_uri = cwd_uri:sub(8)
-      local slash = cwd_uri:find '/'
-      if slash then
-        hostname = cwd_uri:sub(1, slash - 1)
-        -- and extract the cwd from the uri, decoding %-encoding
-        cwd = cwd_uri:sub(slash):gsub('%%(%x%x)', function(hex)
-          return string.char(tonumber(hex, 16))
-        end)
-      end
-    end
+			cwd = cwd_uri.file_path
+			hostname = cwd_uri.host or wezterm.hostname()
+		else
+			-- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+			-- which doesn't have the Url object
+			cwd_uri = cwd_uri:sub(8)
+			local slash = cwd_uri:find("/")
+			if slash then
+				hostname = cwd_uri:sub(1, slash - 1)
+				-- and extract the cwd from the uri, decoding %-encoding
+				cwd = cwd_uri:sub(slash):gsub("%%(%x%x)", function(hex)
+					return string.char(tonumber(hex, 16))
+				end)
+			end
+		end
 
-    -- Remove the domain name portion of the hostname
-    local dot = hostname:find '[.]'
-    if dot then
-      hostname = hostname:sub(1, dot - 1)
-    end
-    if hostname == '' then
-      hostname = wezterm.hostname()
-    end
+		-- Remove the domain name portion of the hostname
+		local dot = hostname:find("[.]")
+		if dot then
+			hostname = hostname:sub(1, dot - 1)
+		end
+		if hostname == "" then
+			hostname = wezterm.hostname()
+		end
 
-    table.insert(cells, cwd)
-    table.insert(cells, hostname)
-  end
+		table.insert(cells, cwd)
+		table.insert(cells, hostname)
+	end
 
-  -- See https://docs.rs/chrono/0.4.19/chrono/format/strftime/index.html
-  local date = wezterm.strftime '%-m/%-d, %a %H:%M'
-  table.insert(cells, date)
+	-- See https://docs.rs/chrono/0.4.19/chrono/format/strftime/index.html
+	local date = wezterm.strftime("%-m/%-d, %a %H:%M")
+	table.insert(cells, date)
 
-  -- An entry for each battery (typically 0 or 1 battery)
-  for _, b in ipairs(wezterm.battery_info()) do
-    table.insert(cells, string.format('%.0f%%', b.state_of_charge * 100))
-  end
+	-- An entry for each battery (typically 0 or 1 battery)
+	for _, b in ipairs(wezterm.battery_info()) do
+		table.insert(cells, string.format("%.0f%%", b.state_of_charge * 100))
+	end
 
-  -- The powerline < symbol
-  local LEFT_ARROW = utf8.char(0xe0b3)
-  -- The filled in variant of the < symbol
-  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	-- The powerline < symbol
+	local LEFT_ARROW = utf8.char(0xe0b3)
+	-- The filled in variant of the < symbol
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 
-  -- Color palette for the backgrounds of each cell
-  local colors = {
-    "#1b2d48",
-    "#2c456b",
-    "#3c649f",
-    "#4779c4",
-    "#83aff0",
-  }
+	-- Color palette for the backgrounds of each cell
+	local colors = {
+		"#1b2d48",
+		"#2c456b",
+		"#3c649f",
+		"#4779c4",
+		"#83aff0",
+	}
 
-  -- Foreground color for the text across the fade
-  local text_fg = '#c0c0c0'
+	-- Foreground color for the text across the fade
+	local text_fg = "#c0c0c0"
 
-  -- The elements to be formatted
-  local elements = {}
-  -- How many cells have been formatted
-  local num_cells = 0
+	-- The elements to be formatted
+	local elements = {}
+	-- How many cells have been formatted
+	local num_cells = 0
 
-  -- Translate a cell into elements
-  function push(text)
-    local cell_no = num_cells + 1
+	-- Translate a cell into elements
+	function push(text)
+		local cell_no = num_cells + 1
 
-    table.insert(elements, { Foreground = { Color = colors[cell_no] } })
-    table.insert(elements, { Text = SOLID_LEFT_ARROW })
+		table.insert(elements, { Foreground = { Color = colors[cell_no] } })
+		table.insert(elements, { Text = SOLID_LEFT_ARROW })
 
-    table.insert(elements, { Foreground = { Color = text_fg } })
-    table.insert(elements, { Background = { Color = colors[cell_no] } })
-    table.insert(elements, { Text = ' ' .. text .. ' ' })
-    num_cells = num_cells + 1
-  end
+		table.insert(elements, { Foreground = { Color = text_fg } })
+		table.insert(elements, { Background = { Color = colors[cell_no] } })
+		table.insert(elements, { Text = " " .. text .. " " })
+		num_cells = num_cells + 1
+	end
 
-  while #cells > 0 do
-    local cell = table.remove(cells, 1)
-    push(cell)
-  end
+	while #cells > 0 do
+		local cell = table.remove(cells, 1)
+		push(cell)
+	end
 
-  window:set_right_status(wezterm.format(elements))
+	window:set_right_status(wezterm.format(elements))
 end)
 
 -- wezterm.on("gui-startup", function(cmd)
