f21a1ac8d59dd64332415f0859bb712934c64104
Hoel Bagard <hoel.bagard@gmail.com>
2025-01-28 14:33:29 +0900

    nvim: add render-markdown

---
+++ .config/nvim/lua/hoel/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        file_types = { "markdown" },
+        render_modes = { "n", "c", "t" },
+        heading = { position = "inline", border = true, border_virtual = true },
+        code = { width = "block", min_width = 45 },
+    },
+}
