225abf8d021a444a36d4963ecf0368b7fb9db89f
Hoel Bagard <hoel.bagard@gmail.com>
2025-02-11 19:27:18 +0900

    zsh: move .zshenv into .config/zsh/env.zsh

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -41,3 +41,19 @@ CORRECT_IGNORE_FILE=".*"
 bindkey '^y' autosuggest-accept
 bindkey '^p' history-search-backward
 bindkey '^n' history-search-forward
+
+# Define pager dependant on what is available (less or more)
+if (( ${+commands[bat]} )); then
+    export PAGER='bat'
+    # Uncomment below to use less as a pager
+    export LESS='-FRSX'
+    export BAT_PAGER="less -iRx4"
+    # Uncomment below to disable pager
+    # export LESS=''
+    # export BAT_PAGER=""
+elif (( ${+commands[less]} )); then
+    export PAGER='less'
+    export LESS='-iRx4'
+elif (( ${+commands[more]} )); then
+    export PAGER='more'
+fi
