7b505a506ffc4a1959cf288c11cdbeb9363e560b
Hoel Bagard <hoel.bagard@gmail.com>
2025-02-17 08:24:10 +0900

    nvim: add dioxus fmt to conform

--- .config/nvim/lua/hoel/plugins/conform.lua
+++ .config/nvim/lua/hoel/plugins/conform.lua
@@ -3,15 +3,23 @@ return {
     "stevearc/conform.nvim",
     lazy = false,
     opts = {
+        -- log_level = vim.log.levels.DEBUG,
         notify_on_error = true,
         format_on_save = {
             timeout_ms = 500,
             lsp_fallback = true,
         },
+        formatters = {
+            dioxus_fmt = {
+                command = "dx",
+                args = { "fmt", "--file", "$FILENAME" },
+                stdin = false,
+            },
+        },
         formatters_by_ft = {
             lua = { "stylua" },
             python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-            rust = { "rustfmt" },
+            rust = { "rustfmt", "dioxus_fmt" },
             markdown = { "prettier" },
             json = { "prettier" },
             javascript = { "prettier" },
