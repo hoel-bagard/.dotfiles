3bc7196f69dd925ea7638a194586badff5ef73a1
Hoel Bagard <hoel.bagard@gmail.com>
2025-02-17 17:26:52 +0900

    zsh: add a warning when fzf is not installed

--- .config/zsh/tools/fzf.zsh
+++ .config/zsh/tools/fzf.zsh
@@ -1,6 +1,8 @@
 if [ -f /usr/share/fzf/key-bindings.zsh ]; then
     source /usr/share/fzf/key-bindings.zsh
     source /usr/share/fzf/completion.zsh
+else
+   echo "Warning: fzf needs to be installed for the auto-complete suggestions to work properly"
 fi
 # The two options below are to have a folder preview when doing cd.
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
