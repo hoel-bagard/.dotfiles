a9b459d4c5f934b7151e94aad2ed6debbb5f180d
Hoel Bagard <hoel.bagard@gmail.com>
2025-05-15 15:04:33 +0900

    nvim: add lazydev as a completion source

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -6,6 +6,13 @@ return {
             "tailwind-tools",
             "onsails/lspkind-nvim",
         },
+        opts = function(_, opts)
+            opts.sources = opts.sources or {}
+            table.insert(opts.sources, {
+                name = "lazydev",
+                group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+            })
+        end,
     },
     {
         "saadparwaiz1/cmp_luasnip",
