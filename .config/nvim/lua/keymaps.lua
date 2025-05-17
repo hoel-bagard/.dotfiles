a7aaf7d09fd8c7ea6997448d9b3820c5c962593c
Hoel Bagard <hoel.bagard@gmail.com>
2025-05-17 22:18:53 +0900

    nvim: keymaps cleanup

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -4,9 +4,12 @@ local icons = require("icons")
 wk.add({
     { "<leader>c", group = "[C]ode" },
     { "<leader>d", group = "[D]ocument" },
+    { "<leader>f", group = "[F]ind" },
     { "<leader>r", group = "[R]ename" },
     { "<leader>s", group = "[S]earch" },
     { "<leader>t", group = "[T]oggle" },
+    { "<leader>x", group = "Diagnostics/Quickfix" },
+    { "<leader>w", group = "[W]orkspace" }, -- TODO: change to windows
 
     {
         "<leader>?",
@@ -24,18 +27,12 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<C-q>", ":xa<CR>", { desc = "[Q]uit all", noremap = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", function()
-    vim.diagnostic.jump({ count = -1 })
-end, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", function()
-    vim.diagnostic.jump({ count = 1 })
-end, { desc = "Go to next [D]iagnostic message" })
+-- stylua: ignore start
+vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-
--- Exit terminal mode in the builtin terminal with a more intuitive shortcut. This does not work, most likely because of the zsh vi-mode plugin.
--- Otherwise, the default shortcut is <C-\><C-n>.
--- vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode", silent = true })
+-- stylua: ignore end
 
 -- Move window focus.
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
@@ -44,24 +41,20 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Change file / buffer
+-- stylua: ignore start
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
-vim.keymap.set("n", "<leader>fa", function()
-    builtin.find_files({ hidden = true, no_ignore = true })
-end, { desc = "[F]ind [A]ll" })
+vim.keymap.set("n", "<leader>fa", function() builtin.find_files({ hidden = true, no_ignore = true }) end, { desc = "[F]ind [A]ll" })
 vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "[F]ind [R]ecent" })
 vim.keymap.set("n", "<leader>fp", builtin.git_files, { desc = "[F]ind [P]roject (git) files" })
--- vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind [G]rep" })
 vim.keymap.set("n", "<leader>fg", require("multigrep").live_multigrep, { desc = "[F]ind [G]rep" })
-vim.keymap.set("n", "<leader>fb", function()
-    builtin.buffers({ sort_mru = true, ignore_current_buffer = true })
-end, { desc = "[F]ind [Buffers]" })
+vim.keymap.set("n", "<leader>fb", function() builtin.buffers({ sort_mru = true, ignore_current_buffer = true }) end, { desc = "[F]ind [Buffers]" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
 vim.keymap.set("n", "<leader>bb", "<C-6>", { desc = "Previous [B]uffer" })
 vim.keymap.set("n", "<leader>pt", ":Neotree<CR>", { desc = "[P]project [T]ree" })
-
 vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete, { desc = "[B]uffer [D]elete" })
 vim.keymap.set("n", "<leader>fs", vim.cmd.update, { desc = "[F]ile [S]ave" })
+-- stylua: ignore end
 
 -- Oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
@@ -83,14 +76,6 @@ vim.keymap.set("x", "<leader>p", '"_dP')
 -- Delete without replacing clipboard.
 vim.keymap.set("n", "<leader>d", '"_d')
 vim.keymap.set("v", "<leader>d", '"_d')
-
--- TODO: useful ?
--- To switch between projects
--- https://youtu.be/w7i4amO_zaE?si=EnRh9FKDKu5SE1Z5&t=1696
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
--- vim.keymap.set("n", "<leader>f", function()
---     vim.lsp.buf.format()
--- end)
 
 -- TODO: Quickfix list
 -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
