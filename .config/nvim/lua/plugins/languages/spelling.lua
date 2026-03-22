4f7d78abf6642f8279512ab32360dcec7e5e2e64
Hoel Bagard <hoel.bagard@gmail.com>
2026-03-22 20:17:48 +0900

    ci: add spelling CI

--- .config/nvim/lua/plugins/languages/spelling.lua
+++ .config/nvim/lua/plugins/languages/spelling.lua
@@ -1,5 +1,5 @@
--- Miscellaneous language tooling
--- LSP: typos_lsp (cross-cutting spell checking for code)
+-- Spelling language tooling
+-- LSP: typos_lsp (source code spell checker)
 
 ---@type LazyPluginSpec[]
 return {
@@ -11,7 +11,7 @@ return {
                 capabilities = capabilities,
                 init_options = {
                     diagnosticSeverity = "hint",
-                    config = vim.env.XDG_CONFIG_HOME .. "/nvim/spell/typos.toml",
+                    config = vim.env.XDG_CONFIG_HOME .. "/typos/typos.toml",
                 },
             })
             vim.lsp.enable("typos_lsp")
