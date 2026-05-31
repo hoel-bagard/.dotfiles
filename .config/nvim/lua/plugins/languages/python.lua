a3a457e23624cf2c22af1a645aadc6c564f77ccd
Hoel Bagard <hoel.bagard@gmail.com>
2026-05-31 21:41:52 +0900

    nvim: re-add pyright

--- .config/nvim/lua/plugins/languages/python.lua
+++ .config/nvim/lua/plugins/languages/python.lua
@@ -14,6 +14,10 @@ return {
         "neovim/nvim-lspconfig",
         opts = function()
             local capabilities = _G.LspCapabilities and _G.LspCapabilities() or {}
+            -- It's fine to have both pyright and basedpyright, so long as only one is installed in the venv. (and they aren't installed system wide)
+            -- Have only one means potentially lacking a LSP depending on the project.
+            vim.lsp.config("pyright", { capabilities = capabilities })
+            vim.lsp.enable("pyright")
             vim.lsp.config("basedpyright", { capabilities = capabilities })
             vim.lsp.enable("basedpyright")
             vim.lsp.config("ruff", { capabilities = capabilities })
